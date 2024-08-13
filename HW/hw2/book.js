bf92506dc0719db0604a58e1896acdaddf3d5f93
SolomiiaKhainatska <khainatska.solomiia09@gmail.com>
2024-08-13 20:18:53 +0200

    add #LARqoUj5I

---
+++ HW/hw2/book.js
@@ -0,0 +1,18 @@
+let book1 = {
+    title: 'Tytanik',
+    pageCount: 456,
+    gerne: 'drama'
+};
+console.log(book1, typeof book1);
+let book2 = {
+    title: 'Quiet Don',
+    pageCount: 890,
+    gerne: 'historical novel'
+};
+console.log(book2, typeof book2);
+let book3 ={
+    title: 'Breackfast at Tiffany',
+    pageCount: 1874,
+    gerne: 'novella'
+};
+console.log(book3, typeof book3);
