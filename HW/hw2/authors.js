926391ac499f5d2ef4a777808fe293a675598f97
SolomiiaKhainatska <khainatska.solomiia09@gmail.com>
2024-08-13 20:58:14 +0200

    add #sA3Gg1sCp

---
+++ HW/hw2/authors.js
@@ -0,0 +1,36 @@
+let book1 = {
+    title: 'Tytanik',
+    pageCount: 456,
+    gerne: 'drama',
+    author: [
+        {
+            name: 'James Cameron',
+            age:43
+        }
+    ]
+};
+console.log(book1, typeof book1);
+let book2 = {
+    title: 'Quiet Don',
+    pageCount: 890,
+    gerne: 'historical novel',
+    author: [
+        {
+            name: 'Mikhail Sholokhov',
+            age: 23
+        }
+    ]
+};
+console.log(book2, typeof book2);
+let book3 ={
+    title: 'Breackfast at Tiffany',
+    pageCount: 1874,
+    gerne: 'novella',
+    author: [
+        {
+            name: 'Truman Capote',
+            age: 34
+        }
+    ]
+};
+console.log(book3, typeof book3);
