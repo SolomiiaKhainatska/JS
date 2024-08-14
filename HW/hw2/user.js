0f608a436a60980a0b34c4ff4b86ee27cac53823
SolomiiaKhainatska <khainatska.solomiia09@gmail.com>
2024-08-14 16:28:55 +0200

    add #jCHFnEbdmFd

---
+++ HW/hw2/user.js
@@ -0,0 +1,22 @@
+let users = [
+    {name:'Vasya', username:'vasya1', password:'pass123'},
+    {name:'Petro', username:'perto2', password:'pass234'},
+    {name:'Taras', username:'taras3', password:'pass345'},
+    {name:'Ivan', username:'ivan4', password:'pass456'},
+    {name:'Mykola', username:'mykola5', password:'pass567'},
+    {name:'Mariya', username:'mariya6', password:'pass678'},
+    {name:'Pavlo', username:'pavlo7', password:'pass789'},
+    {name:'Olga', username:'olga8', password:'pass890'},
+    {name:'Nadiya', username:'nadiya9', password:'pass012'},
+    {name:'Nataliya', username:'nataliya10', password:'pass013'}
+];
+console.log(users[0].password);
+console.log(users[1].password);
+console.log(users[2].password);
+console.log(users[3].password);
+console.log(users[4].password);
+console.log(users[5].password);
+console.log(users[6].password);
+console.log(users[7].password);
+console.log(users[8].password);
+console.log(users[9].password);
