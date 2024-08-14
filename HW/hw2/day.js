896d1ec9f638d1a7eb3a263088c7e9537a967605
SolomiiaKhainatska <khainatska.solomiia09@gmail.com>
2024-08-14 19:47:54 +0200

    add #UMoNq4biWGe

---
+++ HW/hw2/day.js
@@ -0,0 +1,10 @@
+let day = 4;
+if (day >=1 && day <= 10) {
+    console.log('first');
+} else if (day >= 11 && day <= 20) {
+    console.log('second');
+}else if (day >=21 && day <=31) {
+    console.log('third');
+} else {
+    console.log('false');
+}
