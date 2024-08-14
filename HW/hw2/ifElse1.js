a8746877d9aef6666f2c658e2b4b54367976931d
SolomiiaKhainatska <khainatska.solomiia09@gmail.com>
2024-08-14 18:19:25 +0200

    add #bAUsaq6LI

---
+++ HW/hw2/ifElse1.js
@@ -0,0 +1,25 @@
+let x = 8;
+if (x !== 0) {
+    console.log('Вірно');
+} else {
+    console.log('Невірно');
+}
+let a = 1;
+if (a !== 0) {
+    console.log('Вірно');
+} else {
+    console.log('Невірно');
+}
+a = 0;
+if (a !== 0) {
+    console.log('Вірно');
+} else {
+    console.log('Невірно');
+}
+a = -3;
+if (a !== 0)
+{
+    console.log('Вірно');
+} else {
+    console.log('Невірно');
+}
