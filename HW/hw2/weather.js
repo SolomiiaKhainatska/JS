75884ee9e4976f4d5d1de261a80a1178414d744b
SolomiiaKhainatska <khainatska.solomiia09@gmail.com>
2024-08-14 17:27:00 +0200

    add #coYydZuaeEB

---
+++ HW/hw2/weather.js
@@ -0,0 +1,9 @@
+let weather = [
+    {morning: 14, aftenun: 25, evening: 17},
+    {morning: 14, aftenun: 25, evening: 17},
+    {morning: 14, aftenun: 25, evening: 17},
+    {morning: 14, aftenun: 25, evening: 17},
+    {morning: 14, aftenun: 25, evening: 17},
+    {morning: 14, aftenun: 25, evening: 17},
+    {morning: 14, aftenun: 25, evening: 17}
+];
